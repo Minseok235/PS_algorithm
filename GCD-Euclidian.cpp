4dcf9930f6d2dfc30d472289d5896ea15f311ff3
Minseok235 <59851897+Minseok235@users.noreply.github.com>
2020-12-08 03:24:51 -0500

    Update GCD-Euclidian.cpp

--- GCD-Euclidian.cpp
+++ GCD-Euclidian.cpp
@@ -5,3 +5,13 @@ int GCD(int a, int b){
   if (a>b) return GCD(a-b, b);
   else return GCD(b-a, a);
 }
+
+
+//GCD 개선 버전
+int GCD(int a, int b){
+  if (a==0) return b;
+  if (b==0) return a;
+  if (a==b) return a; // or return b;
+  if (a>b) return GCD(a%b, b);
+  else return GCD(b%a, a);
+}
